e63ac433982110c70fdecb2eec926897875e179c
Pedro Igor Fernandes <pedroigorfernandes097@gmail.com>
2025-03-20 21:58:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,19 @@
 exibirTextoNaTela('h1','Jogo de advinhação');
 exibirTextoNaTela('p' ,'Escolha um número entre 1 e 100');
 
+function gerarNumeroAleatorio() {
+   return parseint(Match.random()* 100) + 1;
+}
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto)
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
 function verificarChute() {
-    alert('apertou o botão');
+    let chute = parseint(document.querySelector('imput').value);
+    console.log(chute === numeroSecreto);
 }
 
