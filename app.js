188195578ff4adca1f3d1104a7178aaf123e3c4f
Pedro Igor Fernandes <pedroigorfernandes097@gmail.com>
2025-03-21 19:52:07 -0300

    primeiro codigo

---
+++ app.js
@@ -0,0 +1,28 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo de adivinhação';
+
+// let subtitulo = document.querySelector('p');
+// subtitulo.innerHTML = 'Escolha um número entre 1 e 100';
+
+exibirTextoNaTela('h1', 'Jogo de adivinhação');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10) + 1;
+}
+
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function verificarChute() {
+    let chute = parseInt(document.querySelector('input').value);
+    console.log(chute === numeroSecreto);
+}
+
+//<h1>Jogo de adivinhação</h1>
+//<p>Escolha um número entre 1 e 100</p>
