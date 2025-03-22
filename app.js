ee024fcc57bb924fbe4e450e200db4544f951453
Pedro Igor Fernandes <pedroigorfernandes097@gmail.com>
2025-03-21 21:45:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 // let subtitulo = document.querySelector('p');
 // subtitulo.innerHTML = 'Escolha um número entre 1 e 100';
+let tentativas = 2;
 
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
@@ -21,7 +22,18 @@ function exibirTextoNaTela(tag, texto) {
 
 function verificarChute() {
     let chute = parseInt(document.querySelector('input').value);
-    console.log(chute === numeroSecreto);
+    if (chute === 0) {
+        exibirTextoNaTela('h1', 'suas tentativas acabaram!')
+    } else {
+        tentativas--;
+        if (chute === numeroSecreto) {
+            exibirTextoNaTela('h1', `parabéns! Você acertou! ${numeroSecreto}.`);
+        } else if (chute > numeroSecreto) {
+            exibirTextoNaTela('h1', `Errou! O número secreto é menor. Você ainda tem ${tentativa} ${palavraTentativa}.`);
+        } else {
+            exibirTextoNaTela('h1', `Errou! O número secreto é maior. Você ainda tem ${tentativa} ${tentativas}.`);
+        }
+    }
 }
 
 //<h1>Jogo de adivinhação</h1>
